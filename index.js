35f4264c65af39a246e0ae89259616c9d2429245
steefmin <steefmin@gmail.com>
2016-05-20 10:10:28 +0200

    http server

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
 var Xvfb = require('xvfb')
 var xvfb = new Xvfb()
-xvfb.startSync()
+//xvfb.startSync(function(err,xvfbProcess){
+//  if(err){
+//    console.log(err)
+//  }
+//})
 
-var webtorrent = require('webtorrent-hybrid')
+//var webtorrent = require('webtorrent-hybrid')
+
+//var client = new webtorrent()
+
+var http = require('http')
+
+var port = 7777
+
+var server = http.createServer(handleHttp).listen(port)
+
+function handleHttp (req,res) {
+  console.log(req.url)
+  req.url.replace("/","")
+  res.writeHead(200, {'Content-Type': 'text/plain'})
+  res.write('booster started')
+  res.end()
+}
+
+//xvfb.stopSync()
